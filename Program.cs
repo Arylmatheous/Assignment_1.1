eb2710ae40400599466f5dd391fc4c46a6bfcb89
Arylmatheous <arylmatheous22@gmail.com>
2024-03-27 23:52:50 +0800

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Item1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 3;
+            while (counter != 0)
+            {
+                Console.Write("Enter your username: ");
+                string username = Console.ReadLine();
+
+                Console.Write("Enter your password: ");
+                string password = Console.ReadLine();
+
+                if (username == "Admin" && password == "Admin1234!")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Login is successful!");
+                    break;
+                    
+                }
+                else
+                {
+                    Console.Clear();
+                    counter --;
+                    Console.WriteLine($"Login is unsuccessful! Remaining Attempts: {counter}");
+                }
+            }
+
+
+        }
+    }
+}
